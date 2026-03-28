93489b8a6077354f9d922814058d9e49479a91ad
websample.steamspace <65386045+iyus123@users.noreply.github.com>
2026-03-28 17:16:11 +0700

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,4 +6,13 @@ const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceRoleKey);
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  supabaseServiceRoleKey,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+    },
+  }
+);
